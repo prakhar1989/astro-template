c773c37dbb728b30abd16fcab844e044a9825110
Vova Kryachko <vkryachko@google.com>
2024-03-12 15:52:26 +0000

    Add clone template

---
+++ clone/idx-template.nix
@@ -0,0 +1,34 @@
+{pkgs, repo, ...}: 
+let 
+  devNix = pkgs.writeText "dev.nix" ''
+    {pkgs, ...}: {
+        packages = [
+            pkgs.python3
+        ];
+
+        idx.workspace.onCreate = {
+            init = '''
+              ./git-cookie-authdaemon && \
+                  git clone ${repo} /home/user/repo && \
+                  code -r /home/user/repo
+            ''';
+        };
+    }
+  '';
+in {
+    packages = [
+        pkgs.coreutils
+        pkgs.curl
+        pkgs.gzip
+        pkgs.gnutar
+    ];
+
+    bootstrap = ''
+      mkdir -p "$out/.idx"
+      REV="6c05256"
+      curl -L https://github.com/GerritCodeReview/gcompute-tools/tarball/"$REV" | tar -xz
+      sed 's/metadata.google.internal/metadata.google.internal:8080/' -i GerritCodeReview-gcompute-tools-"$REV"/git-cookie-authdaemon
+      install --mode u+rwx GerritCodeReview-gcompute-tools-"$REV"/git-cookie-authdaemon "$out"
+      install --mode u+rw ${devNix} "$out"/.idx/dev.nix
+    '';
+}
