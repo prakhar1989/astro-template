d32670d7e7b6be6ba63fc2d44756367f99c44727
Vladimir Kryachko <vkryachko@google.com>
2024-04-10 16:01:12 -0400

    Update idx-template.nix

--- fl/idx-template.nix
+++ fl/idx-template.nix
@@ -1,9 +1,10 @@
 { pkgs, ... }: {
   packages = [ pkgs.nodejs_20 pkgs.bubblewrap ];
   bootstrap = ''
+    printenv
     npm config ls
     date
-    bwrap --unshare-net --bind / / --dev /dev npx --offline --loglevel verbose -y @angular/cli new --skip-git --defaults --skip-install --directory "$WS_NAME" "$WS_NAME"
+    npx --offline --loglevel verbose -y @angular/cli new --skip-git --defaults --skip-install --directory "$WS_NAME" "$WS_NAME"
     date
     mkdir "$WS_NAME"/.idx
     cp ${./dev.nix} "$WS_NAME"/.idx/dev.nix && chmod +w "$WS_NAME"/.idx/dev.nix
